707e100d2f995f0524a0f7b508d04303ee641315
Devon Jones <djones9898@hotmail.com>
2016-02-22 19:02:04 -0900

    add listing model add devise gem

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_djones!, only:[:create, :edit, :update, :destroy]
   # GET /listings
   # GET /listings.json
   def index
